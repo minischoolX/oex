e90783433c96d36146e719181f995f4f09066683
minischoolX <104305605+minischoolX@users.noreply.github.com>
2022-10-03 22:47:26 +0530

    Update EdxWebViewPool.java

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/cache/EdxWebViewPool.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/cache/EdxWebViewPool.java
@@ -2,7 +2,7 @@ package org.edx.mobile.view.custom.cache;
 
 import android.content.Context;
 import android.content.MutableContextWrapper;
-import androidx.core.util.Pools;
+import androidx.util.Pools;
 
 import org.edx.mobile.view.custom.cache.utils.LogUtils;
 import org.edx.mobile.view.custom.EdxWebView;

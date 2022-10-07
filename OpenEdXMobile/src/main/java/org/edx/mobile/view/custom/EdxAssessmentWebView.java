7d84319549e579b3596ca784b00aa9d04afc2196
minischoolX <minischoolx@outlook.com>
2022-10-07 11:25:12 +0530

    Update EdxAssessmentWebView.java

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/EdxAssessmentWebView.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/EdxAssessmentWebView.java
@@ -4,8 +4,16 @@ import android.content.Context;
 import android.util.AttributeSet;
 
 public class EdxAssessmentWebView extends EdxWebView {
+
+    private Context mContext;
+
     public EdxAssessmentWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
         getSettings().setSupportZoom(false);
     }
+
+    public getContext(Context context) {
+        mContext = context;
+    }
+
 }

5c3f3e2967e64b8759a3a4f0aa1ea294f8769710
minischoolX <minischoolx@outlook.com>
2022-10-11 11:07:07 +0530

    xxxx

--- OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/cache/okhttp/OkHttpClientProvider.java
+++ OpenEdXMobile/src/main/java/org/edx/mobile/view/custom/cache/okhttp/OkHttpClientProvider.java
@@ -5,22 +5,10 @@ import android.content.Context;
 import org.edx.mobile.view.custom.cache.cookie.FastCookieManager;
 
 import java.io.File;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-
-import org.edx.mobile.http.interceptor.GzipRequestInterceptor;
-import org.edx.mobile.http.interceptor.OauthHeaderRequestInterceptor;
-
-//import org.edx.mobile.BuildConfig;
-//import org.edx.mobile.R;
-//import org.edx.mobile.http.authenticator.OauthRefreshTokenAuthenticator;
-//import org.edx.mobile.http.interceptor.NewVersionBroadcastInterceptor;
-//import org.edx.mobile.http.interceptor.OauthHeaderRequestInterceptor;
-//import org.edx.mobile.http.interceptor.UserAgentInterceptor;
 
 /**
  * Created by Ryan
@@ -32,8 +20,6 @@ public class OkHttpClientProvider {
     private static final int OKHTTP_CACHE_SIZE = 100 * 1024 * 1024;
     private static volatile OkHttpClientProvider sInstance;
     private OkHttpClient mClient;
-
-//    private OauthRefreshTokenAuthenticator oauthRefreshTokenAuthenticator;
 
     private OkHttpClientProvider(Context context) {
         createOkHttpClient(context);
@@ -52,37 +38,17 @@ public class OkHttpClientProvider {
 
 
     private void createOkHttpClient(Context context) {
-
-//        this.oauthRefreshTokenAuthenticator = oauthRefreshTokenAuthenticator;
-
         String dir = context.getCacheDir() + File.separator + CACHE_OKHTTP_DIR_NAME;
-        OkHttpClient.Builder mClientBuilder = new OkHttpClient.Builder();
-
-                List<Interceptor> interceptors = mClientBuilder.interceptors();
-                interceptors.add(new GzipRequestInterceptor());
-                interceptors.add(new OauthHeaderRequestInterceptor(context));
-
-                mClientBuilder.cookieJar(FastCookieManager.getInstance().getCookieJar(context));
-                mClientBuilder.cache(new Cache(new File(dir), OKHTTP_CACHE_SIZE));
-                mClientBuilder.readTimeout(20, TimeUnit.SECONDS);
-                mClientBuilder.writeTimeout(20, TimeUnit.SECONDS);
-                mClientBuilder.connectTimeout(20, TimeUnit.SECONDS);
+        mClient = new OkHttpClient.Builder()
+                .cookieJar(FastCookieManager.getInstance().getCookieJar(context))
+                .cache(new Cache(new File(dir), OKHTTP_CACHE_SIZE))
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .connectTimeout(20, TimeUnit.SECONDS)
                 // auto redirects is not allowed, bc we need to notify webview to do some internal processing.
-                mClientBuilder.followSslRedirects(false);
-                mClientBuilder.followRedirects(false);
-
-//                .addInterceptor(new UserAgentInterceptor(
-//                        System.getProperty("http.agent") + " " +
-//                                context.getString(R.string.app_name) + "/" +
-//                                BuildConfig.APPLICATION_ID + "/" +
-//                                BuildConfig.VERSION_NAME))
-//                .addInterceptor(new OauthHeaderRequestInterceptor(context))
-//                .addInterceptor(oauthRefreshTokenAuthenticator)
-//                .addInterceptor(new NewVersionBroadcastInterceptor())
-//                .authenticator(oauthRefreshTokenAuthenticator)
-
-//                mClient.build();
-                mClient = mClientBuilder.build();
+                .followSslRedirects(false)
+                .followRedirects(false)
+                .build();
     }
 
     public static OkHttpClient get(Context context) {
